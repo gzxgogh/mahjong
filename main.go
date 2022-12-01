36e1758d87c3d77aa4def885501a3dc499fce131
gogh <1215043129@qq.com>
2022-12-01 18:01:46 +0800

    修改为结构体

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 
-	var cardsArr, newCardsArr []model.Card
+	var totalCardsArr, finalCardsArr []model.Card
 	typeArr := []model.CardType{model.CardType_W, model.CardType_T, model.CardType_S}
 	for _, item := range typeArr {
 		for i := 0; i < 4; i++ {
 			for value := 1; value <= 9; value++ {
-				cardsArr = append(cardsArr, model.Card{
+				totalCardsArr = append(totalCardsArr, model.Card{
 					Type:  item,
 					Value: value,
 				})
@@ -21,18 +21,29 @@ func main() {
 		}
 	}
 	for i := 0; i < 4; i++ {
-		cardsArr = append(cardsArr, model.Card{
+		totalCardsArr = append(totalCardsArr, model.Card{
 			Type:  model.CardType_Z,
 			Value: 1,
 		})
 	}
-
 	for i := 112; i > 0; i-- {
 		randomNum := utils.GetRandomWithAll(0, i-1)
-		newCardsArr = append(newCardsArr, cardsArr[randomNum])
-		cardsArr = append(cardsArr[:randomNum], cardsArr[(randomNum+1):]...)
+		finalCardsArr = append(finalCardsArr, totalCardsArr[randomNum])
+		totalCardsArr = append(totalCardsArr[:randomNum], totalCardsArr[(randomNum+1):]...)
 	}
-	fmt.Println("newCardsArr", len(newCardsArr), newCardsArr)
+	var groupA, groupB, groupC, groupD []model.Card
+	for i, item := range finalCardsArr {
+		if i < 28 {
+			groupA = append(groupA, item)
+		} else if i >= 28 && i < 56 {
+			groupB = append(groupB, item)
+		} else if i >= 56 && i < 84 {
+			groupC = append(groupC, item)
+		} else if i >= 84 && i < 112 {
+			groupD = append(groupD, item)
+		}
+	}
+	fmt.Println("newCardsArr", len(finalCardsArr), finalCardsArr)
 
 	//redis.InItRedisCoon()
 	//service.ShuffleCards(1001, 3, "player1")
